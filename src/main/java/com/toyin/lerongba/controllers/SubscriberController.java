ecb1f8884ac9d328829465e02391f0546985d728
BToyin <72198080+BToyin@users.noreply.github.com>
2022-12-14 16:19:08 +0000

    Configured the subscription controller so that you can now add new subscriber to DB and it displays the message of success or error

--- src/main/java/com/toyin/lerongba/controllers/SubscriberController.java
+++ src/main/java/com/toyin/lerongba/controllers/SubscriberController.java
@@ -4,6 +4,7 @@ import com.toyin.lerongba.entities.Subscriber;
 import com.toyin.lerongba.services.SubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,32 +25,31 @@ public class SubscriberController {
         this.subscriberService = subscriberService;
     }
 
-
     @PostMapping("/homepage/subscribe")
     public RedirectView saveSubscriber(@ModelAttribute Subscriber subscriber, RedirectAttributes redirectAttrs, HttpServletRequest request) {
 
         if (subscriberService.existsByEmail(subscriber.getEmail())) {
-            redirectAttrs.addFlashAttribute("error", "Email address already subscribed");
-            return new RedirectView("redirect:/homepage/subscribe",true);
+            redirectAttrs.addFlashAttribute("error", "Error! Email address already subscribed");
+        } else {
+            subscriberService.createSubscriber(subscriber);
+            redirectAttrs.addFlashAttribute("passed", "Email address added as subscriber successfully!");
         }
-        subscriberService.createSubscriber(subscriber);
-        redirectAttrs.addFlashAttribute("passed", "Email address added as subscriber successfully!");
-        return new RedirectView("redirect:/homepage/subscribe",true);
+        return new RedirectView("/homepage/subscribe",true);
     }
 
     @GetMapping("/homepage/subscribe")
-    public String getHomePageAfterSubmission(HttpServletRequest request) {
+    public String getHomePageAfterSubmission(HttpServletRequest request, Model model) {
         Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(request);
         if (inputFlashMap != null) {
             if (inputFlashMap.containsKey("passed")) {
                 String passed = (String) inputFlashMap.get("passed");
+                model.addAttribute("passed", passed);
             } else {
                 String error = (String) inputFlashMap.get("error");
+                model.addAttribute("error", error);
             }
-            return "homepage";
-        } else {
-            return "redirect:/homepage";
         }
+        model.addAttribute("subscriber", new Subscriber());
+        return "/homepage";
     }
-
 }
